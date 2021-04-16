f8b2ea19b2562850c8903c2d87c36d1e410b42ae
Aaron Lou <al968@cornell.edu>
2021-04-16 00:57:10 -0600

    correct template save name

--- run/alexnet_template.sh
+++ run/alexnet_template.sh
@@ -2,11 +2,11 @@
 #SBATCH -N 1
 #SBATCH --mem=15000
 #SBATCH -t 24:00:00
-#SBATCH --partition=default_gpu
+#SBATCH --partition=cuvl
 #SBATCH --gres=gpu:1
 #SBATCH -o results_alexnet/outputs/%j.out
 #SBATCH -e results_alexnet/outputs/%j.err
 
 source activate TopNetwork
 
-python main.py --save_dir results_alexnet/$DATASET_$LR_$BATCH_$SEED --dataset $DATASET --model alexnet --lr $LR --batch_size_train $BATCH --seed $SEED
+python main.py --save_dir results_alexnet/${DATASET}_${LR}_${BATCH}_${SEED} --dataset $DATASET --model alexnet --lr $LR --batch_size_train $BATCH --seed $SEED
