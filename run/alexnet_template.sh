822c387b79c7145dc5eee02fdac2bad5ac834dca
Aaron Lou <al968@cornell.edu>
2021-04-15 22:02:20 -0600

    maybe this'll work?

--- run/alexnet_template.sh
+++ run/alexnet_template.sh
@@ -7,7 +7,6 @@
 #SBATCH -o outputs/%j.out
 #SBATCH -e outputs/%j.err
 
-source ~/.bashrc
 source activate TopNetwork
 
 python main.py --save_dir results_alexnet/{$DATASET}_{$LR}_{$BATCH}_{$SEED} --dataset {$DATASET} --model alexnet --lr {$LR} --bs {$BATCH} --seed {$SEED}
