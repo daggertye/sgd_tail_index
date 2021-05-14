930823dc5e1dd3cd20bc58a4a5049c6c2e14c0d9
Aaron Lou <al968@cornell.edu>
2021-05-14 12:30:03 -0600

    update

--- main.py
+++ main.py
@@ -211,8 +211,9 @@ if __name__ == '__main__':
             evaluation_history_TEST.append([i + 1, *te_hist]) 
             evaluation_history_TRAIN.append([i + 1, *tr_hist])
 
-            weights_history = torch.stack(tuple(weights_history)).numpy()
-            ph_dim = calculate_ph_dim(weights_history)
+            weights_history_np = torch.stack(tuple(weights_history)).numpy()
+            del weights_history
+            ph_dim = calculate_ph_dim(weights_history_np)
 
             test_acc = evaluation_history_TEST[-1][1]
             train_acc = evaluation_history_TRAIN[-1][1]
