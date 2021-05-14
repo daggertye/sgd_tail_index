4ac4d3abf63be527d8f22fd983217d6a3473a2c0
Aaron Lou <al968@cornell.edu>
2021-05-14 12:05:05 -0600

    different vgg partition

--- run/vgg/template.sh
+++ run/vgg/template.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1
 #SBATCH --mem=128000
 #SBATCH -t 48:00:00
-#SBATCH --partition=cuvl
+#SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH -o results_vgg/outputs/%j.out
 #SBATCH -e results_vgg/outputs/%j.err
