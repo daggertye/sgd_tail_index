591f7cf624dcb3d182201fd8f75209fe9f86b846
Aaron Lou <al968@cornell.edu>
2021-05-22 03:30:10 -0600

    typos

--- run/alexnet_new/manual_sweep.py
+++ run/alexnet_new/manual_sweep.py
@@ -9,10 +9,8 @@ modulo = int(sys.argv[2])
 
 assert modulo > mod
 
-save_file = 'dims_alexnet_new.txt'
-
 # experimental setup
-datasets = ['MNIST', 'cifar10']
+datasets = ['mnist', 'cifar10']
 seeds = list(range(1)) #list(range(3))
 learning_rates = [1e-3, 5e-3]
 batch_size = [64, 128, 256]
@@ -22,7 +20,7 @@ grid = itertools.product(datasets, seeds, learning_rates, batch_size)
 i = 0
 for d, s, lr, bs, in grid:
     if i % modulo == mod:
-        cmd = f"python main.py --save_file dims_alexnet_optimizers.txt --meta_data {d}_{lr}_{bs}_{s} --dataset {d} --model alexnet --lr {lr} --batch_size_train {bs} --seed {s}"
+        cmd = f"python main.py --save_file dims_alexnet_new.txt --meta_data {d}_{lr}_{bs}_{s} --dataset {d} --model alexnet --lr {lr} --batch_size_train {bs} --seed {s}"
         print(cmd)
         os.system(cmd)
     i += 1
